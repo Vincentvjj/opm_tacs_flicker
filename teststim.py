8b7bd04bf7cfe73452d22732ca3c29ee95c47cb1
Vincent Jonany <vincentjonany@Vincents-MacBook-Air.local>
2023-02-03 12:18:09 +0100

    new

--- teststim.py
+++ teststim.py
@@ -4,39 +4,39 @@ import matplotlib.pyplot as plt
 import time
 from scipy import signal
 
-sfreq = 24000
+sfreq = 1000
 
 target_freq = 10 # 10hz
-carrier_freq = 220 # 40hz
+carrier_freq = 200 # 40hz
 
-amplitude = 0.5
-duration = 3 # 10 s
+amplitude = 2
+duration = 10 # 10 s
 t_samples = np.arange(duration*sfreq)
 carrier = np.sin(2 * np.pi * carrier_freq * t_samples/sfreq) * amplitude
 modulator = np.sin(2 * np.pi * target_freq * t_samples/sfreq) 
 # am_stim = amplitude * 0.5 * (carrier * modulator / 2 ) #1mA p2p
 am_stim = (amplitude + modulator * amplitude) * carrier /2 #100 % modulation index
-# am_stim = (amplitude+0.1 + modulator * 0.9) * carrier /2 #90 % modulation index
+# am_stim = (amplitude + modulator * amplitude*100)  #90 % modulation index
 
 
 
 
 plt.figure()
-plt.plot(am_stim[:])
+plt.plot(am_stim[:1000])
+plt.show()
+
+plt.figure()
+f, pxx = signal.welch(am_stim, sfreq, nperseg=sfreq)
+plt.plot(f, pxx)
+plt.xlim(0, 230)
 plt.show()
 
 # plt.figure()
-# f, pxx = signal.welch(am_stim, sfreq, nperseg=sfreq/4)
-# plt.semilogy(f, pxx)
-# plt.xlim(0, 50)
+# sp = np.fft.fft(am_stim)
+# trange = np.linspace(0, sfreq, len(am_stim))
+# plt.plot(trange, np.abs(sp))
+# plt.xlim(0,80)
 # plt.show()
-
-plt.figure()
-sp = np.fft.fft(am_stim)
-trange = np.linspace(0, sfreq, len(am_stim))
-plt.plot(trange, np.abs(sp))
-plt.xlim(0,80)
-plt.show()
 
 
 
